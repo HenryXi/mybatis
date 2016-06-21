b0ee63f4ec88da5a0023fd114b39cf2a19c28fd7
yong <xxy668@foxmail.com>
2016-06-21 13:46:21 +0800

    use idea mybatis plugin. code finish

---
+++ generator/etc/Use IDEA Mybatis plugin generate mapper files.md
@@ -0,0 +1,14 @@
+# Use IDEA Mybatis plugin generate mapper files
+Mybatis is a light weight Java persistence framework(compared with Hibernate). You have to write
+a lot of mapper xml files and mapper interfaces. These things are duplicate works, we can generate
+them by IDEA Mybatis plugin. In this page I will show you how to use this plugin to generate xml mapper
+file and mapper interface.
+
+1. install Mybatis plugin
+
+    Search Mybatis plugin in Intellij repositories and click install.
+    ![IDEA Mybatis plugin](http://a.disquscdn.com/uploads/mediaembed/images/3829/5262/original.jpg)
+2. create mybatis-generator-config file
+    Right click resources folder choose **New** | **mybatis-generator-config**.
+    ![IDEA Mybatis plugin](http://a.disquscdn.com/uploads/mediaembed/images/3829/6805/original.jpg)
+3. 
---
+++ generator/src/main/java/com/henryxi/mybatis/Client.java
@@ -0,0 +1,36 @@
+package com.henryxi.mybatis;
+
+import com.henryxi.mybatis.entity.UserInfoEntity;
+import com.henryxi.mybatis.entity.UserInfoEntityExample;
+import com.henryxi.mybatis.mapper.UserInfoEntityMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            UserInfoEntityMapper mapper = sqlSession.getMapper(UserInfoEntityMapper.class);
+            UserInfoEntity userInfoEntity = new UserInfoEntity();
+            userInfoEntity.setId(UUID.randomUUID().toString());
+            userInfoEntity.setName("HenryXi");
+            mapper.insert(userInfoEntity);
+            UserInfoEntityExample example = new UserInfoEntityExample();
+            example.createCriteria().andNameEqualTo("HenryXi");
+            List<UserInfoEntity> allRecords = mapper.selectByExample(example);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
